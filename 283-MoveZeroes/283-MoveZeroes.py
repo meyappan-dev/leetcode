189176cb83d4cb293f9f05d80dd7e8c0e16f9515
Meyappan Valliappan <meyappan2942@gmail.com>
2025-01-12 02:34:52 -0500

    Update file 1/12/2025, 2:34:52 AM

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -6,8 +6,6 @@ class Solution(object):
         """
         i=0
         for j in range(len(nums)):
-            # if(nums[i] != 0):
-            #     i+=1
             if(nums[j] != 0):
                 temp = nums[i]
                 nums[i] = nums[j]
