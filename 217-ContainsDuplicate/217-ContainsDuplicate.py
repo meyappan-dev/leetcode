d16da76c170a6a7f203a51565203dec98cc47ffb
Meyappan Valliappan <meyappan2942@gmail.com>
2025-01-18 19:38:15 -0500

    Update file 1/18/2025, 7:38:15 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -2,11 +2,12 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # nums.sort()
         res = {}
+
         for i in range(len(nums)):
             # j=i+1
             # while j<len(nums):
             # if nums[i]==nums[i+1]:
-            if res.get(nums[i])==True:
+            if nums[i] in res:
                 return True
             else:
                 res[nums[i]] = True
