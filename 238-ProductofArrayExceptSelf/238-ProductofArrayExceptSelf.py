7b996f50821f37aac363812fee09307eed9ab2c9
Meyappan Valliappan <meyappan2942@gmail.com>
2025-01-20 22:48:36 -0500

    Update file 1/20/2025, 10:48:36 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,17 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # temp1=nums[0]
+        # temp1=1
+        # answer = [1] * len(nums)
+        # for i in range(len(nums)):
+        #     answer[i] = temp1
+        #     temp1 = nums[i]*temp1        
+        # temp1=1
+        # for j in reversed(range(len(nums))):
+        #     answer[j] = answer[j]*temp1
+        #     temp1 = nums[j]*temp1
+        # return answer
+
+        res=[1]*len(nums)
+        j=len(nums)-2
+        temp=1
         temp1=1
-        answer = [1] * len(nums)
-        for i in range(len(nums)):
-            # if i>0:
-            answer[i] = temp1
-            temp1 = nums[i]*temp1
-        
-        # temp1=nums[len(nums)-1]
-        temp1=1
-        for j in reversed(range(len(nums))):
-            # if j < len(nums)-1:
-            answer[j] = answer[j]*temp1
-            temp1 = nums[j]*temp1
-        return answer
+        for i in range(1,len(nums)):
+            temp1=nums[i-1]*temp1
+            res[i]=temp1*res[i]
+            while j >=0:
+                temp=nums[j+1]*temp
+                res[j]=temp*res[j]
+                j-=1
+        return res
