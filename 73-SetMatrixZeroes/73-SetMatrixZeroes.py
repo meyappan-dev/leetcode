ae8e31d0bb9926a6e1301c81f4241742deab1084
Meyappan Valliappan <meyappan2942@gmail.com>
2025-03-20 17:18:36 -0400

    Create file

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        dupRow = set()
+        dupCol = set()
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    dupRow.add(i)
+                    dupCol.add(j)
+
+        for i in dupRow:
+            for j in range(n):
+                matrix[i][j]=0
+        
+        for j in dupCol:
+            for i in range(m):
+                matrix[i][j]=0
+
+        return matrix
