5c3556d674500afa3fc469ca5fed0cddd004f605
Meyappan Valliappan <meyappan2942@gmail.com>
2025-04-04 01:21:18 -0400

    File updated at 4/4/2025, 1:21:17 AM

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.py
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.py
@@ -1,28 +1,13 @@
-# Last updated: 3/25/2025, 2:43:37 AM
+# Last updated: 4/4/2025, 1:21:17 AM
 class Solution:
-    def calculateSum(self,numRows: int, baseRow: List[int],resFinal = List[List[int]]) -> List[List[int]]:
-        res2=[]
-        for num in range(3,numRows+1):
-            for i in range(len(baseRow)):
-                if i == 0:
-                    res2.append(baseRow[i])
-                if i == len(baseRow)-1:
-                    res2.append(baseRow[i])
-                if i+1 <= len(baseRow)-1:    
-                    dummy = baseRow[i]+baseRow[i+1]
-                    res2.append(dummy)
-            resFinal.append(res2)
-            print(res2)
-            baseRow=res2.copy()
-            res2=[]
-        return resFinal
-
     def generate(self, numRows: int) -> List[List[int]]:
-        res=[]
-        if numRows >= 1:
-            res.append([1])
-        if numRows > 1:
-            res.append([1,1])
-        if numRows > 2:
-            res = self.calculateSum(numRows,[1,1],res)
+        res=[[1]]
+        nextRow=[]
+
+        for i in range(1,numRows):
+            temp = [0] + res[-1] + [0]
+            for j in range(i+1):
+                nextRow.append(temp[j]+temp[j+1])
+            res.append(nextRow)
+            nextRow=[]
         return res
